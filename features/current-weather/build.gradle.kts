ac83bbe858b95a5d438c36c260a57e14523f5da8
Tanany <itstanany@gmail.com>
2025-01-04 21:33:34 +0200

    chore/remove unused data module dependency from features submodules

--- features/current-weather/build.gradle.kts
+++ features/current-weather/build.gradle.kts
@@ -38,7 +38,6 @@ android {
 
 dependencies {
   implementation(project(":core"))
-  implementation(project(":data"))
   implementation(project(":domain"))
 
   implementation(libs.androidx.core.ktx)
--- features/forecast/build.gradle.kts
+++ features/forecast/build.gradle.kts
@@ -38,12 +38,11 @@ android {
 
 dependencies {
   implementation(project(":core"))
-  implementation(project(":data"))
+  implementation(project(":domain"))
 
   implementation(libs.androidx.core.ktx)
   implementation(libs.androidx.appcompat)
   implementation(libs.material)
-  implementation(project(":domain"))
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
